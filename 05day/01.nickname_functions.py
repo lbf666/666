b093c23e390b0c4089b399dc8f1073596287e3e2
hlions <hlions@163.com>
2019-10-29 11:09:06 +0800

    nickname functions

--- 05day/01.nickname_functions.py
+++ 05day/01.nickname_functions.py
@@ -1,2 +1,15 @@
+# 函数可以赋值给一个变量
+def add(a=0, b=0):
+    return a + b
+
+
+result = add
+print(result(1, 2))
+
 # 匿名函数: lambda variable: expression
+# y = ax + b -> ax + b - y = 0
+adds = lambda x, y: 3*x + y + 10
+print(adds(3, 4))
+
+
 
